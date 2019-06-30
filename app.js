36913275dd5a70fae4c0b2ce50c204395cba6e7d
yinchengnuo <yinnuo96@163.com>
2019-06-30 08:24:03 +0800

    add-liveplayingschemes

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const bodyParser = require('koa-bodyparser');
 const connectMongodb = require('./util/connectMongodb');
 
 (async() => {
-    // try {
-    //     await connectMongodb
-    // } catch {
-    //     console.log('数据库连接失败！！')
-    //     return
-    // }
-    // console.log('数据库连接成功！！！')
+    try {
+        await connectMongodb
+    } catch {
+        console.log('数据库连接失败！！')
+        return
+    }
+    console.log('数据库连接成功！！！')
 
     const app = new Koa();
 
@@ -21,6 +21,7 @@ const connectMongodb = require('./util/connectMongodb');
     require('./resumeIndexJquery/app')(app); //主页简历
     require('./fuliaoLiveAuditRecord/app')(app); //富聊直播审核记录
     require('./fuliaoLiveWithoutFlash/app')(app); //富聊后台视频聊优化建议方案
+    require('./livePlayingSchemes/app')(app); //web前端常用直播流播放方案（PC端）
     require('./fuliaoVideochatAuditRecord/app')(app); //富聊视频聊复查快捷工具
     require('./fuliaoWebappVue/app')(app); //富聊webapp移动端Vue开发版
     require('./test/app')(app); //测试页面
