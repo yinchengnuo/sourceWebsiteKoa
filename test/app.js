ff2a3afd5a9f3bd68368faa05a9fd467813ae0ca
yinchengnuo <yinnuo96@163.com>
2019-09-03 18:09:39 +0800

    c

--- test/app.js
+++ test/app.js
@@ -5,8 +5,42 @@ const { readFilePromise } = require('../util/promisify');
 
 module.exports = (app) => {
 
+    const custom = 'x-custom-header'
+
+    let key = 'TOKENTOKENTOKEN'
+
     router.get('/', async(ctx) => {
         ctx.body = (await readFilePromise(__dirname + '/index.html')).toString()
+    })
+
+    router.post('/login', async(ctx) => {
+        ctx.response.set(custom, key)
+        ctx.body = {
+            code: 200,
+            message: '登陆成功'
+        }
+    })
+
+    router.post('/other', async(ctx) => {
+        console.log(ctx.request.headers[custom])
+        if (ctx.request.headers[custom] === key) {
+            ctx.body = {
+                code: 200,
+                message: '操作成功'
+            }
+        } else {
+            ctx.body = {
+                code: 501,
+                message: '未登录'
+            }
+        }
+    })
+
+    router.post('/logout', async(ctx) => {
+        ctx.body = {
+            code: 200,
+            message: '退出成功'
+        }
     })
 
     app.use(router.routes()).use(router.allowedMethods());
