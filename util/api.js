aa7da75579ff5d79a5932a193740cd69e4d9b5a1
yinchengnuo <yinnuo96@163.com>
2019-09-17 13:58:15 +0800

    add keeppace

--- util/api.js
+++ util/api.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const util = require('util');
 const axios = require('axios');
 const router = require('koa-router')();
+const unlinkPromise = util.promisify(fs.unlink);
+const readdirPromise = util.promisify(fs.readdir);
+const readFilePromise = util.promisify(fs.readFile);
+const writeFilePromise = util.promisify(fs.writeFile);
 
 const host = 'https://api.96friend.cn';
 const query1 = 'apptype=6';
@@ -11,6 +15,23 @@ const query4 = 'pagesize=14'
 const query5 = 'cversion=29011505'
 
 module.exports = app => {
+
+    router.post('/keeppace', async ctx => {
+        await writeFilePromise('../mockApiKoa/app.js', ctx.request.body.app)
+        const { dir: { name, app, apis } } = ctx.request.body;
+        await writeFilePromise(`../mockApiKoa/${name}/app.js`, app);
+        const files = await readdirPromise(`../mockApiKoa/${name}/apis/`)
+        for(let i = 0; i < files.length; i++) {
+            await unlinkPromise(`../mockApiKoa/${name}/apis/${files[i]}`);
+        }
+        const apikeys = Object.keys(apis);
+        for(let i = 0; i < apikeys.length; i ++) {
+            await writeFilePromise(`../mockApiKoa/${name}/apis/${apikeys[i]}`, apis[apikeys[i]]);
+        }
+        ctx.body = {
+            code: 200
+        }
+    })
 
     router.get('/tuijian', async ctx => {
         ctx.body = (await axios(`${host}/videoLive!getRecommendList.htm?${query1}&${query2}&${query3}&${query4}&${query5}&pageno=${ctx.query.page}&_timestamp=${Math.floor(+new Date() / 1000)}`)).data
